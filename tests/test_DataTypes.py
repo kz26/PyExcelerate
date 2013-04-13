493d18b9b6fd2d2e4653916c22fa329854e07707
whitehat2k9 <whitehat2k9@gmail.com>
2013-04-12 21:57:46 -0500

    Fixed test_DataTypes

--- tests/test_DataTypes.py
+++ tests/test_DataTypes.py
@@ -5,10 +5,10 @@ def test__to_enumeration_value():
     eq_(DataTypes.to_enumeration_value(DataTypes.BOOLEAN), "b")
     eq_(DataTypes.to_enumeration_value(DataTypes.DATE), "d")
     eq_(DataTypes.to_enumeration_value(DataTypes.ERROR), "e")
-	eq_(DataTypes.to_enumeration_value(DataTypes.INLINE_STRING), "inlineStr")
-	eq_(DataTypes.to_enumeration_value(DataTypes.NUMBER), "n")
-	eq_(DataTypes.to_enumeration_value(DataTypes.SHARED_STRINGS), "s")
-	eq_(DataTypes.to_enumeration_value(DataTypes.STRING), "str")
+    eq_(DataTypes.to_enumeration_value(DataTypes.INLINE_STRING), "inlineStr")
+    eq_(DataTypes.to_enumeration_value(DataTypes.NUMBER), "n")
+    eq_(DataTypes.to_enumeration_value(DataTypes.SHARED_STRING), "s")
+    eq_(DataTypes.to_enumeration_value(DataTypes.STRING), "str")
 
 def test__get_type():
     eq_(DataTypes.get_type(15), DataTypes.NUMBER)
