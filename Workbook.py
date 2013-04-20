032f15c0f34fac8efa41400c2a7c929a7d19ca91
Kevin Wang <kevmo314@gmail.com>
2013-04-20 05:45:17 -0500

    bug fix

--- Workbook.py
+++ Workbook.py
@@ -10,8 +10,8 @@ class Workbook(object):
     def add_sheet(self, worksheet):
         self._worksheets.append(worksheet)
         
-    def new_sheet(self, sheet_name):
-        worksheet = Worksheet.Worksheet(sheet_name, self)
+    def new_sheet(self, sheet_name, data=None):
+        worksheet = Worksheet.Worksheet(sheet_name, self, data)
         self._worksheets.append(worksheet)
         return worksheet
 
