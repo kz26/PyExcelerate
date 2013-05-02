1786f17af8bb938ad4b175817cd64488bdd41ea2
Kevin Zhang <whitehat2k9@gmail.com>
2013-05-02 12:51:04 -0500

    Updated and refactored tests/benchmark.py

--- pyexcelerate/tests/benchmark.py
+++ pyexcelerate/tests/benchmark.py
@@ -3,17 +3,17 @@ import openpyxl
 import xlsxwriter.workbook
 import time
 
-
-def test_benchmark():
-    ROWS = 65000
-    COLUMNS = 100
+ROWS = 65000
+COLUMNS = 100
+testData = [[1] * COLUMNS] * ROWS
+def run_pyexcelerate():
     wb = Workbook()
-    testData = [[1] * COLUMNS] * ROWS
     stime = time.clock()
     ws = wb.new_sheet("Test 1", data=testData)
     wb.save("test.xlsx")
     print "pyexcelerate, %s, %s, %s" % (ROWS, COLUMNS, time.clock() - stime)
     
+def run_openpyxl():
     stime = time.clock()
     wb = openpyxl.workbook.Workbook(optimized_write=True) 
     ws = wb.create_sheet()
@@ -23,11 +23,17 @@ def test_benchmark():
     wb.save("test.xlsx")
     print "openpyxl, %s, %s, %s" % (ROWS, COLUMNS, time.clock() - stime)
 
+def run_xlsxwriter():
     stime = time.clock()
-    wb = xlsxwriter.workbook.Workbook('test.xlsx', {'reduce_memory': 1})
+    wb = xlsxwriter.workbook.Workbook('test.xlsx', {'constant_memory': True})
     ws = wb.add_worksheet()
     for row in range(ROWS):
         for col in range(COLUMNS):
             ws.write_number(row, col, 1)
     wb.close()
     print "xlsxwriter, %s, %s, %s" % (ROWS, COLUMNS, time.clock() - stime)
+
+def test_all():
+    run_pyexcelerate()
+    run_xlsxwriter()
+    run_openpyxl()
