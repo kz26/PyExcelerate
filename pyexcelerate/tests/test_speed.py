2eedf20dffd3537940d27ae0b91adcf40a3ded29
kz26 <whitehat2k9@gmail.com>
2013-10-07 10:44:41 -0500

    Update speed test and travis config

--- pyexcelerate/tests/test_speed.py
+++ pyexcelerate/tests/test_speed.py
@@ -4,4 +4,4 @@ from nose.tools import ok_
 def test_vs_xlsxwriter():
 	ours = run_pyexcelerate()
 	theirs = run_xlsxwriter()
-	ok_(ours / theirs <= 0.5, msg='PyExcelerate is too slow! Better Excelerate it some more!')
+	ok_(ours / theirs <= 0.67, msg='PyExcelerate is too slow! Better Excelerate it some more!')
