cd59d3bdd5ba924bb103b7c9ea7d65a1d2aceb8e
kz26 <whitehat2k9@gmail.com>
2014-05-12 14:52:32 -0500

    Update PyExcelerate hook

--- hook-pyexcelerate.py
+++ hook-pyexcelerate.py
@@ -5,10 +5,7 @@ PyInstaller hook for PyExcelerate
 
 import os
 
-try:
-	from pyexcelerate.Writer import _TEMPLATE_PATH
-	datas = [
-		(os.path.join(_TEMPLATE_PATH, '*'), 'pyexcelerate/templates')
-	]
-except ImportError:
-	pass
+from pyexcelerate.Writer import _TEMPLATE_PATH
+datas = [
+	(os.path.join(_TEMPLATE_PATH, '*'), 'pyexcelerate/templates')
+]
