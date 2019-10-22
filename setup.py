727265cc67b8fbef2ce342c31996d648ffff9456
Kevin Wang <kevmo314@gmail.com>
2019-10-22 12:07:28 +0200

    Update setup.py classifiers.

--- setup.py
+++ setup.py
@@ -34,11 +34,11 @@ setup(
 		'Intended Audience :: Developers',
 		'License :: OSI Approved :: BSD License',
 		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3.3',
 		'Programming Language :: Python :: 3.4',
 		'Programming Language :: Python :: 3.5',
 		'Programming Language :: Python :: 3.6',
 		'Programming Language :: Python :: 3.7'
+		'Programming Language :: Python :: 3.8',
 	]
 
 )
