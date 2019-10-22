e015d34fd1c83b2ca04ca0a5bb07396ee289fc6c
Kevin Wang <kevmo314@gmail.com>
2019-10-22 10:25:22 +0200

    Add Python 3.7 to setup.py classifiers
    #83

--- setup.py
+++ setup.py
@@ -33,12 +33,12 @@ setup(
 		'Development Status :: 4 - Beta',
 		'Intended Audience :: Developers',
 		'License :: OSI Approved :: BSD License',
-		'Programming Language :: Python :: 2.6',
 		'Programming Language :: Python :: 2.7',
 		'Programming Language :: Python :: 3.3',
 		'Programming Language :: Python :: 3.4',
 		'Programming Language :: Python :: 3.5',
-		'Programming Language :: Python :: 3.6'
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7'
 	]
 
 )
